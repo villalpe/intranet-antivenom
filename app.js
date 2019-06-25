1fde7b1384a7017ebf1dfa2d8701577c04febc8f
Eduardo Villalpando <villalpe5@gmail.com>
2019-06-25 08:38:23 -0500

    First Total Update

---
+++ app.js
@@ -0,0 +1,53 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+//cargar rutas
+var user_routes = require('./routes/user');
+var animal_routes = require('./routes/animal');
+var invoice_routes = require('./routes/invoice');
+var poll_routes = require('./routes/poll');
+var uploadfile_routes = require('./routes/uploadfile');
+var fileud_routes = require('./routes/fileud');
+var invhw_routes = require('./routes/invhw');
+
+//var appRoutes = require('./routes/app');
+//var fileRoutes = require('./routes/file');
+
+//middleware de body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+//Convertir lo que traiga el body a json
+app.use(bodyParser.json());
+
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
+
+
+//rutas base
+app.use('/', express.static('client', {redirect:false}));
+app.use('/api', user_routes);
+app.use('/api', animal_routes);
+app.use('/api', invoice_routes);
+app.use('/api', poll_routes);
+app.use('/api', uploadfile_routes);
+app.use('/api', fileud_routes);
+app.use('/api', invhw_routes);
+
+app.get('*', function(req, res, next) {
+	res.sendfile(path.resolve('client/index.html'));
+});
+
+//Exportar modulo
+module.exports = app;
+
